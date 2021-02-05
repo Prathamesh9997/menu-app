cc24341dc9a6964c5060ef2203d1d05672ad5a4d
Prathamesh Gunde <65587388+Prathamesh9997@users.noreply.github.com>
2021-02-05 11:44:44 +0530

    final-commit-menu

---
+++ src/Categories.js
@@ -0,0 +1,23 @@
+import React from "react";
+
+const Categories = ({ categories, filteredItems }) => {
+  return (
+    <div className="btn-container">
+      {categories.map((category, index) => {
+        return (
+          <button
+            type="button"
+            className="filter-btn"
+            onClick={() => {
+              filteredItems(category);
+            }}
+          >
+            {category}
+          </button>
+        );
+      })}
+    </div>
+  );
+};
+
+export default Categories;
